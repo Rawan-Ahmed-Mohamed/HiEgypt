088c6abb8e2a8dd7053e93e9ef06284bafb96de9
gehad179 <143453005+gehad179@users.noreply.github.com>
2024-04-28 23:47:37 +0300

    Update resturants.js

--- JAVA FILES_2/resturants.js
+++ JAVA FILES_2/resturants.js
@@ -36,19 +36,19 @@ window.onload = function() {
       return false; 
   } else {
       if (searchInput.toLowerCase() === 'buffalo'||searchInput.toLowerCase() === 'food') {
-          window.location.href = 'restuarants.htm#buffalo';
+          window.location.href = 'restuarants.html#buffalo';
           return false; 
       }
       if (searchInput.toLowerCase() === 'Desserts') {
-          window.location.href = 'restuarants.htm#Desserts'; 
+          window.location.href = 'restuarants.html#Desserts'; 
           return false;
       }
       if (searchInput.toLowerCase() === 'special-dish'||searchInput.toLowerCase()==='special dish') {
-        window.location.href = 'restuarants.htm#special-dish'; 
+        window.location.href = 'restuarants.html#special-dish'; 
         return false; 
     }
     if(searchInput.toLowerCase() === 'drinks'||searchInput.toLowerCase()==='coffee') {
-      window.location.href = 'restuarants.htm#CAFE'; 
+      window.location.href = 'restuarants.html#CAFE'; 
       return false; }
       else{
           document.getElementById("searchInput").style = "color:red;";
