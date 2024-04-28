d9d40b9cfc2d8904e596e42471047930261740fc
Rahma Shaban <166531192+Rahma-Shaban@users.noreply.github.com>
2024-04-28 16:55:27 +0300

    Update store.js

--- javascript files/store.js
+++ javascript files/store.js
@@ -19,24 +19,26 @@ buyButtons.forEach(function(button) {
         alert("Stay tuned for our new products!");
     }
 
-    document.addEventListener('DOMContentLoaded', function () {
-        const searchInput = document.getElementById('searchInput');
-        const cards = document.querySelectorAll('.card');
-    
-        searchInput.addEventListener('input', function () {
-            const searchText = searchInput.value.toLowerCase();
-    
-            cards.forEach(card => {
-                const desc = card.querySelector('.desc').textContent.toLowerCase();
-                if (searchText === "accessories" && desc.includes("accessories")) {
-                    card.classList.add('highlight');
-                } else if (searchText === "clothes" && desc.includes("clothes")) {
-                    card.classList.add('highlight');
-                } else if (searchText === "statue" && desc.includes("statue")) {
-                    card.classList.add('highlight');
-                } else {
-                    card.classList.remove('highlight');
-                }
-            });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const searchInput = document.getElementById('searchInput');
+    const cards = document.querySelectorAll('.card');
+
+    searchInput.addEventListener('input', function () {
+        const searchText = searchInput.value.toLowerCase();
+
+        cards.forEach(card => {
+            const desc = card.querySelector('.desc').textContent.toLowerCase();
+            if (searchText === "accessories" && desc.includes("accessories")) {
+                card.classList.add('highlight');
+            } else if (searchText === "clothes" && desc.includes("clothes")) {
+                card.classList.add('highlight');
+            } else if (searchText === "statue" && desc.includes("statue")) {
+                card.classList.add('highlight');
+            } else {
+                card.classList.remove('highlight');
+            }
         });
     });
+});
