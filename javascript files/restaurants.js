52c570ee395866126980c508cb1c34339005002f
gehad179 <143453005+gehad179@users.noreply.github.com>
2024-04-30 12:27:36 +0300

    Update restaurants.js

--- javascript files/restaurants.js
+++ javascript files/restaurants.js
@@ -2,14 +2,13 @@ window.onload = function() {
     var popup = document.querySelector('.popup');
     popup.style.display = 'block';
   
-    // إخفاء المسدج بعد فترة زمنية قصيرة باستخدام animate.css
     setTimeout(function() {
       popup.classList.remove('animate__fadeIn');
-      popup.classList.add('animate__fadeOut'); // استخدم animate__fadeOut للاختفاء
+      popup.classList.add('animate__fadeOut'); 
       setTimeout(function() {
         popup.style.display = 'none';
-      }, 1000); // زمن الانتقال في animate.css هو 1 ثانية
-    }, 1500); // زمن انتظار قبل بدء عملية الانتقال هو 2 ثوانٍ
+      }, 1000); 
+    }, 1500); 
   };
 
   
